2d503cdb884889b0a3f30ddfae9b77488bd8203e
Hristo <hristo.anastasov@gmail.com>
2022-08-12 16:42:19 +0300

    untitled-app initial version

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, HammerModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MasterViewComponent } from './master-view/master-view.component';
+import { IgxListModule, IgxAvatarModule, IgxIconModule } from 'igniteui-angular';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MasterViewComponent
+  ],
+  imports: [
+    BrowserModule,
+    HammerModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    IgxListModule,
+    IgxAvatarModule,
+    IgxIconModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
